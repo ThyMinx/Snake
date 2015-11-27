7d32d0b7f5fb2c21e6207923b576279606fa086f
avifo <avifo98@gmail.com>
2015-11-27 15:47:17 +0000

    Movement
    Player can now move. However, they can only move left.

---
+++ Snake/Assets/SnakeMovement.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SnakeMovement : MonoBehaviour
+{
+
+    public float speed = 1.75f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+    }
+}
+
