3f9d4b806b88e33f176286ed951db0286634f2fc
ThyMinx <james.cairns@outlook.com>
2015-11-27 14:46:43 +0000

    James - Project Folder
    Contains all the content from my copy.

---
+++ Snake/Assets/Scripts/Game Scripts/GameManager.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+    public bool paused = false;
+    public int score = 0;
+
+    public GameObject pauseMenu;
+
+    public Text scoreText;
+
+    void Start()
+    {
+        score = 0;
+        pauseMenu.SetActive(false);
+    }
+
+    void OnGUI()
+    {
+        scoreText.text = "Score: " + score;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("p"))
+        {
+            if (paused)
+            {
+                pauseMenu.SetActive(false);
+                paused = false;
+                Time.timeScale = 1;
+            }
+            else
+            {
+                pauseMenu.SetActive(true);
+                paused = true;
+                Time.timeScale = 0;
+            }
+        }
+    }
+
+}
