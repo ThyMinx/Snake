f4334ae9ed9052e83c485340bfe0add764487748
avifo <avifo98@gmail.com>
2015-11-27 17:04:41 +0000

    Move
    Play can now move, borders work

--- Snake/Assets/SnakeMovement.cs
+++ Snake/Assets/SnakeMovement.cs
@@ -4,16 +4,31 @@ using System.Collections;
 public class SnakeMovement : MonoBehaviour
 {
 
-    public float speed = 1.75f;
+    public float speed = 0.5f;
+    public float TurnSpeed = 10f;
 
 
     // Update is called once per frame
     void Update()
     {
+
+        transform.localPosition += transform.up * speed * Time.deltaTime;
+
         if (Input.GetKey(KeyCode.LeftArrow))
         {
+
             transform.position += Vector3.left * speed * Time.deltaTime;
         }
-    }
-}
 
+        {
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                transform.position += Vector3.right * speed * Time.deltaTime;
+       
+            }
+        }
+
+        
+    }
+
+}
